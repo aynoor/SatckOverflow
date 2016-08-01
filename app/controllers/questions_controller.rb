a915f4c88e93265ec195a0a5c91f83f0337168fa
aynoor <aynoorsaleem@ymail.com>
2016-08-01 11:46:38 +0500

    Minor Error fixed: Question Model initialized.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,6 @@ class QuestionsController < ApplicationController
 
   def index
     @questions = Question.paginate(page: params[:page])
-    @users = User.paginate(page: params[:page])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726082749) do
+ActiveRecord::Schema.define(version: 20160801064256) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20160726082749) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "description"
+    t.string   "explanation"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
