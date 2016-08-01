5aff7d6f7ca05e2a86e385a48e477d435d8852dc
aynoor <aynoorsaleem@ymail.com>
2016-08-01 11:37:25 +0500

    Initial project: Minor errors present.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:secret]
   
   def index
-    @users = User.paginate(page: params[:page])
-    @pages = Page.paginate(page: params[:page])
+    
   end
   
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,8 @@
 class QuestionsController < ApplicationController
-  
+
   def index
     @questions = Question.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
 end
